91c00816b1dfffe174c4269d9f42d5ab307f4e81
Miguel Mur Cortés <mmur@ucm.es>
2021-10-03 12:09:37 +0200

    p1

---
+++ FicherosP1/Mytar/mytar.h
@@ -0,0 +1,24 @@
+#ifndef _MYTAR_H
+#define _MYTAR_H
+
+#include <limits.h>
+
+typedef enum{
+  NONE,
+  ERROR,
+  CREATE,
+  EXTRACT
+} flags;
+
+typedef struct {
+  char* name;
+  unsigned int size;
+} stHeaderEntry;
+
+const int BUFF_SIZE;
+
+int createTar(int nFiles, char *fileNames[], char tarName[]);
+int extractTar(char tarName[]);
+
+
+#endif /* _MYTAR_H */
