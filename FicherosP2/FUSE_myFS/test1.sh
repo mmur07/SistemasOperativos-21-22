dc63a324b9f0c7d259d5a04c0af47560b78f92e9
Miguel Mur Cortés <mmur@ucm.es>
2022-01-02 18:06:54 +0100

    p2 terminada

--- FicherosP2/FUSE_myFS/test1.sh
+++ FicherosP2/FUSE_myFS/test1.sh
@@ -20,7 +20,44 @@ read -p "Press enter..."
 echo "Disk auditioning"
 ./my-fsck virtual-disk
 if diff ./test/myFS.h $MPOINT/myFS.h
-then echo echo $"CORRECTO: Son iguales :)"
+then echo $"CORRECTO: Son iguales :)"
 else echo $"FALLO: No son iguales :("
 fi
+read -p "Press enter..."
 
+echo "Comparing truncated files"
+./my-fsck virtual-disk
+truncate --size=4096 $MPOINT/myFS.h
+truncate --size=4096 test/myFS.h
+
+if diff ./test/myFS.h $MPOINT/myFS.h
+then echo $"CORRECTO: Son iguales :)"
+else echo $"FALLO: No son iguales :("
+fi
+read -p "Press enter..."
+
+echo "Copying file 3"
+cp ./src/fuseLib.h $MPOINT/
+cp ./src/fuseLib.h ./test
+read -p "Press enter..."
+
+echo "Comparing file 3"
+./my-fsck virtual-disk
+if diff ./test/fuseLib.h $MPOINT/fuseLib.h
+then echo $"CORRECTO: Son iguales :)"
+else echo $"FALLO: No son iguales :("
+fi
+read -p "Press enter..."
+
+echo "Truncating file 3"
+./my-fsck virtual-disk
+truncate --size=+4096 $MPOINT/fuseLib.h
+truncate --size=+4096 test/fuseLib.h
+read -p "Press enter..."
+
+echo "Comparing file 3"
+./my-fsck virtual-disk
+if diff ./test/fuseLib.h $MPOINT/fuseLib.h
+then echo $"CORRECTO: Son iguales :)"
+else echo $"FALLO: No son iguales :("
+fi
